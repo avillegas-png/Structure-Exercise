e620ef7c518ef247ad58904c4cac0a8f489d203c
avillegas-png <67976032+avillegas-png@users.noreply.github.com>
2020-07-23 14:42:47 -0500

    Add files via upload

---
+++ Alan_V_StructureExercise.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+struct engineTypes {
+    var engine1 = "V4"
+    var engine2 = "V6"
+    var engine3 = "V8"
+}
+
+let engineChoice = engineTypes()
+
+print("The engine options you have are a \(engineChoice.engine1) engine, a \(engineChoice.engine2) engine, and a \(engineChoice.engine3) engine")
+
